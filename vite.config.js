eec8cab4998a083a5ca8b1bd3d1341d40ab822e2
lin15158385 <lin15158385@gmail.com>
2026-01-10 23:25:40 +0000

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/PWD-Astro_Runner/", // <--- Adicionado para deployment no GitHub Pages
   plugins: [
     react({
       babel: {
