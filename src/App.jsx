78afb278cd94aff6d7545d1d18243d3e899884ad
lin15158385 <lin15158385@gmail.com>
2026-01-11 03:29:44 +0000

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,6 @@ import Game from '../game/game.jsx';
 import { fetchNEOs } from '../game/nasa.js';
 
 export default function App() {
-  /*const canvasRef = React.useRef(null);
-
-  React.useEffect(() => {
-    console.log("App.jsx rodando");
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-
-    // tamanho inicial
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    // resize
-    const resize = () => {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    };
-    window.addEventListener('resize', resize);
-
-    // NASA → GLOBAL (como o jogo espera)
-    fetchNEOs().then(neos => {
-      window.neoCache = neos;
-    });
-
-    return () => {
-      window.removeEventListener('resize', resize);
-    };
-  }, []);
-  */
     return (
     <Game />
   );
