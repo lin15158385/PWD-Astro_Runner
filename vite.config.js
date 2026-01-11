e286e1b0ae13a6b3077c0408f2fb09489369293b
lin15158385 <lin15158385@gmail.com>
2026-01-11 01:38:41 +0000

    deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/PWD-Astro_Runner/",
+  base: '/PWD-Astro_Runner/',
   plugins: [react()],
 })
